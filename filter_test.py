79681829bfc0d44d86d799f5766441b073b9eac6
ykoneee <245403188@qq.com>
2018-06-01 20:16:07 +0800

    change code structure

--- filter_test.py
+++ filter_test.py
@@ -1,111 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from filterpy.monte_carlo  import systematic_resample
-from filterpy.kalman import UnscentedKalmanFilter
-from filterpy.common import Q_discrete_white_noise
-from filterpy.kalman import MerweScaledSigmaPoints
-import scipy.stats
-import time
-from numpy.random import randn,uniform
-class fake_robot():
-    def __init__(self,sensor_std=0.1,control_std=0.001):
-        self.a=0
-        self.v=1.5
-        self.angle=0
-        self.x=0
-        self.y=0
-        self.state=0
-        self.state_holdcount=60
-        self.dt=0.05
-        self.sensor_std=sensor_std
-        self.control_std=control_std
-        self.rotate_acc=0
-    def sample(self):
-        self.state_holdcount+=1
-        if self.state_holdcount>90:
-            self.state_holdcount=0
-            if uniform()<0.8:
-                tempnum1=np.random.randint(0,3)
-                while self.state==tempnum1:
-                    tempnum1=np.random.randint(0,3)
-                self.state=tempnum1
-            if self.state==0:
-                self.v=0.8+uniform()
-                self.rotate_acc=0
-            elif self.state==1:
-                self.v=0.7+uniform(0,1)
-                if uniform()<0.5:
-                    self.rotate_acc=np.deg2rad(uniform(4,12))
-                else:
-                    self.rotate_acc=np.deg2rad(-uniform(4,12))
-            elif self.state==2:
-                self.v=0
-                self.rotate_acc=0
-        
-        real_control_std=0 if self.state==2 else self.control_std
-        self.angle+=self.rotate_acc*self.dt+randn()*.06*real_control_std
-        dist=self.v*self.dt+abs(randn())*real_control_std
-        self.x+=np.cos(self.angle)*dist
-        self.y+=np.sin(self.angle)*dist
-        return self.v,self.rotate_acc,\
-               self.x,self.y,self.angle,\
-               self.x+randn()*self.sensor_std,self.y+randn()*self.sensor_std,self.angle+randn()*0.01*self.sensor_std
-class pf():
-    def __init__(self,init_point,std):
-        self.N=1000
-        self.particles=self.normal_particles(init_point,std,self.N)
-        #self.particles=self.uniform_particles([-2,2],[-2,2],[1,2],self.N)
-        self.weights = np.zeros(self.N)
-        self.resample_time=0
-    def uniform_particles(self,x_range, y_range, v_range, N):
-        particles = np.empty((N, 3))
-        particles[:, 0] = np.random.uniform(x_range[0], x_range[1], size=N)
-        particles[:, 1] = np.random.uniform(y_range[0], y_range[1], size=N)
-        particles[:, 2] = np.random.uniform(v_range[0], v_range[1], size=N)
-        particles[:, 2] %= 2 * np.pi
-        return particles
-    def normal_particles(self,mean, std, N):
-        particles = np.empty((N, 3))
-        particles[:, 0] = mean[0] + (np.random.randn(N) * std[0])
-        particles[:, 1] = mean[1] + (np.random.randn(N) * std[1])
-        particles[:, 2] = mean[2] + (np.random.randn(N) * std[2])
-        particles[:, 2] %= 2 * np.pi
-        return particles    
-    def predict(self, u, std, dt=1.,angle_increase_mode=True):
-        
-        if angle_increase_mode:
-            self.particles[:, 2] += u[1]*dt + (np.random.randn(self.N) * std[1])
-        else:
-            self.particles[:, 2] = u[1] + (np.random.randn(self.N) * std[1])
-        self.particles[:, 2] %= 2 * np.pi          
 
-        dist = (u[0] * dt) + (np.random.randn(self.N) * std[0])
-        self.particles[:, 0] += np.cos(self.particles[:, 2]) * dist
-        self.particles[:, 1] += np.sin(self.particles[:, 2]) * dist  
-    def update(self,z,std):
-        z=np.array(z)
-        distance=np.linalg.norm(self.particles[:,0:2]-z, axis=1)
-        self.weights=scipy.stats.norm(distance,std).pdf(0)
-        self.weights+=1.e-300
-        self.weights/=np.sum(self.weights)
-    def estimate(self):
-        pos = self.particles[:, 0:2]
-        mean = np.average(pos, weights=self.weights, axis=0)
-        var  = np.average((pos - mean)**2, weights=self.weights, axis=0)
-        return mean, var    
-    def neff(self):
-        return 1. / np.sum(np.square(self.weights))  
-    def resample_from_index(self,indexes):
-        self.particles[:] = self.particles[indexes]
-        self.weights[:] = self.weights[indexes]
-        self.weights.fill (1.0 / len(self.weights))    
-    def resample(self):
-        if self.neff() < self.N/2:
-            #print('resample!')
-            self.resample_time+=1
-            indexes = systematic_resample(self.weights)
-            self.resample_from_index(indexes)     
+import time
+from robot_utils import fake_robot,calculate_filter_error
+from particle_filter import pf
+from unscented_kalman_filter import get_ukf
 
 robot=fake_robot(sensor_std=.14,control_std=.004)
 origin_data=np.array([robot.sample() for _ in range(5000)])
@@ -121,17 +21,7 @@ def refrashdata():
     move_no_noise=origin_data[:,2:5]
     move_with_noise=origin_data[:,5:]
     move_with_filter=None
-def calculate_filter_error(name,subname):
-    a=move_no_noise[:,:2]
-    b=move_with_filter
-    if b.shape[1]!=2:
-        b=b[:,:2]
-    err=np.linalg.norm(a-b,axis=1)
-    plt.subplot(subname)
-    plt.xlim(0,0.5)
-    sns.distplot(err,bins=300)
-    plt.vlines(err.mean(), 0, 100,'r')
-    print(f'{name} err:avg:{err.mean():.3f} std:{err.std():.3f} max:{err.max():.3f} min:{err.min():.3f}')
+
 def pftest(a='121'):
     global move_with_filter
     plt.subplot(a)
@@ -163,26 +53,10 @@ def pftest(a='121'):
     plt.grid(True)
     ax=plt.gca()
     ax.set_aspect(1)
-    calculate_filter_error('pf',a+1)
+    calculate_filter_error('pf',a+1,move_no_noise,move_with_filter)
 def ukftest(a=221):
     global move_with_filter
-    def f_cv(x, dt,v_angle):
-        v=v_angle[0]
-        rad=v_angle[1]
-        x[2]+=rad*dt
-        x[2]%=2*np.pi    
-        dist = (v*dt)
-        x[0]+= np.cos(x[2]) * dist
-        x[1]+= np.sin(x[2]) * dist          
-        return x
-    def h_cv(x):
-        return x[:2]
-    sigmas = MerweScaledSigmaPoints(3, alpha=.1, beta=2., kappa=0.)
-    ukf = UnscentedKalmanFilter(dim_x=3, dim_z=2, fx=f_cv,
-              hx=h_cv, dt=0.05, points=sigmas)
-    ukf.x = np.array([0., 0.,0.])
-    ukf.R *= 0.05
-    ukf.Q = Q_discrete_white_noise(3, dt=0.05, var=0.1)
+    ukf=get_ukf()
     uxs = []  
     t1=time.time()
     for i in range(len(move_with_noise)):
@@ -201,7 +75,7 @@ def ukftest(a=221):
     plt.grid(True)
     ax=plt.gca()
     ax.set_aspect(1)    
-    calculate_filter_error('ukf',a+1)
+    calculate_filter_error('ukf',a+1,move_no_noise,move_with_filter)
 def ukf_aug_search(R=5,Q_var=0.1,alpha=0.05):
     global move_with_filter
     def f_cv(x, dt,v_angle):
